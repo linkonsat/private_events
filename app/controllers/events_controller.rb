f6a0d0c3a407fe7efaa78ff561bfcae45b50f90a
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2022-02-02 10:22:50 -0800

    Added controller redirects for form submission.:

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,22 @@
 class EventsController < ApplicationController
     def index
     end
+    def new
+        @event = Event.new
+    end
+
+    def show 
+    end
+    def create 
+        if @event.save 
+           redirect_to action: show, @post 
+        end
+        
+    end
+    def index
+    end
+    private 
+    def post_params
+        params.require(:event).permit(:name, :description, :creator, :event_date, :event_end_date, :user_id)
+    end
 end
