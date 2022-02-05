8e82f35ee913b631693d980fb9791017c678093a
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2022-02-04 22:22:19 -0800

    Fixed associations to include events from user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :events
   has_many :event_attendees, foreign_key: :attendee_id
   has_many :attendee, through: :event_attendee
+  has_many :events, through: :event_attendees
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
