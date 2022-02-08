4a8bdc2456d8b66c3fae1fd9e21c9b1aee9f97d9
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2022-02-08 08:41:35 -0800

    Added new views for events.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         before_create do |user|
+          puts "about to create #{user.user_name}"
+        end
+    
+        after_create :just_created
+    
+        private
+    
+        def just_created
+          puts "just created a user"
+        end
 end
