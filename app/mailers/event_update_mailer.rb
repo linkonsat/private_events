0fd4484db7c60420d44ef2475b2cfe0248630382
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2022-02-27 21:36:39 -0800

    Added new mailer template update.

--- app/mailers/event_update_mailer.rb
+++ app/mailers/event_update_mailer.rb
@@ -2,7 +2,7 @@ class EventUpdateMailer < ApplicationMailer
     def event_update(params)
         @emails = params[:emails]
         @emails.each do |email|
-                mail(to: email, subject: 'Booking confirmation')
+                mail(to: email, subject: 'Event info change')
         end
 
       end
