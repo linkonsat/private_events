3e7a164a38a1b44bc30fef3a412f2dfbf7774051
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2022-02-27 23:17:19 -0800

    Add email sending for production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # SMTP settings for gmail
+config.action_mailer.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :user_name            => "linkonsat",
+  :password             => "ljpwdnczopgphznl",
+  :authentication       => "plain",
+  :enable_starttls_auto => true
+}
 end
