0bbc5233d0262329bd8d4a7445a6414955588d94
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2022-03-06 00:51:16 -0800

    Removed sensitive info.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,8 +97,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    user_name: 'linkonsat',
-    password: 'ljpwdnczopgphznl',
+    user_name: ENV['gmail_username'],
+    password: ENV['gmail_app_password'],
     authentication: 'plain',
     enable_starttls_auto: true
   }
