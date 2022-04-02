3b16b8164750ddd6b5aab6bca562ead2cf73f597
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2022-04-01 19:40:33 -0700

    Added event tag model and related files.

--- db/migrate/20220402023927_create_event_tags.rb
+++ db/migrate/20220402023927_create_event_tags.rb
@@ -1,9 +1,10 @@
-class AddEventHashTags < ActiveRecord::Migration[7.0]
+class CreateEventTags < ActiveRecord::Migration[7.0]
   def change
     create_table :event_tags do |t|
       t.string :name 
       t.belongs_to :event, foreign_key: true
       t.belongs_to :user, foreign_key: true
+      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_02_022451) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_02_023927) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -64,6 +64,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_02_022451) do
     t.string "name"
     t.bigint "event_id"
     t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["event_id"], name: "index_event_tags_on_event_id"
     t.index ["user_id"], name: "index_event_tags_on_user_id"
   end
