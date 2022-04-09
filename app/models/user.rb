c71a2eb54ac0fbb3d5f4f77a212b8b099636cf12
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2022-04-09 09:24:57 -0700

    Added user name and email validation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  validates :email, uniqueness: { message: 'Email is already taken. Please select another. ' }
+  validates :user_name, uniqueness: { message: 'User name is already taken. Please select another. ' }
   has_many :events
   has_many :event_invites
   # Include default devise modules. Others available are:
