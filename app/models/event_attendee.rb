30cf19f297230475eebcc761052483ece8f783a6
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2022-04-18 22:06:45 -0700

    Wrote event attendee search method.

--- app/models/event_attendee.rb
+++ app/models/event_attendee.rb
@@ -4,6 +4,7 @@ class EventAttendee < ApplicationRecord
   belongs_to :attendee, class_name: 'User'
   belongs_to :event
   validate :one_user_signup
+  scope :signed_up_user?, ->(params) { where('event_id = ?', params[:event_id]).and(where('attendee_id = ?', params[:attendee_id])) }
 
     def one_user_signup 
       sign_up = EventAttendee.where('event_id = ?', self.event_id).and(EventAttendee.where('attendee_id = ?', self.attendee_id))
