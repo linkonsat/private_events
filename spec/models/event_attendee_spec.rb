7b4d92c54b3347fb9b499ff203c7f262f4de6db4
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2022-04-18 22:06:24 -0700

    Wrote added event attendee model unit test.

--- spec/models/event_attendee_spec.rb
+++ spec/models/event_attendee_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe EventAttendee, type: :model do
     confirm_attendee = new_attendee.save
     expect(confirm_attendee).to eql(false)
   end
+  it "Model scope method returns a signed up event user" do 
+    user = create(:user)
+    event = create(:event)
+    new_attendee = EventAttendee.create!(:id => 1, :attendee_id => user.id, :event_id => event.id, :created_at => Time.now, :updated_at => Time.now)
+    params_hash = { :event_id => event.id, :attendee_id => user.id }
+    attendee = EventAttendee.signed_up_user?(params_hash)
+    expect(attendee.empty?).to eql(false)
+  end
 end
