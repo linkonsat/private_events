0f7f7a07d4412b9af4067dfba23f1140f81080ab
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2022-04-24 20:16:24 -0700

    Added promo code model and associations.

--- app/models/promo_code.rb
+++ app/models/promo_code.rb
@@ -1,3 +1,19 @@
 class PromoCode < ApplicationRecord
     belongs_to :reward
+
+    scope :find_code, ->(code) { where('code = ?', code) }
+
+  def self.generate_promo_code 
+    promo_code = ""
+    6.times do 
+        char_number = rand(25) + 97
+        promo_code << char_number.chr
+    end
+    debugger
+    if (PromoCode.find_code(promo_code).empty?)
+        return promo_code 
+    else
+        self.generate_promo_code 
+    end 
+  end
 end
