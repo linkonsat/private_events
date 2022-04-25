59ba4e1750bbdeb1484718de5d428b60c318a274
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2022-04-24 20:15:58 -0700

    Added new promo code model test.

---
+++ spec/models/promo_code_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe PromoCode, type: :model do
+  it "Returns a promocode" do 
+    promo_code = PromoCode.generate_promo_code
+    expect(promo_code).to_not eql(nil)
+  end
+end
